a1ba029c9976e9b36ffa0e6d97e8393a9425a6b3
mstavares <hmigueltavares@gmail.com>
2015-05-02 16:26:26 +0100

    Monitor ok

--- computer.h
+++ computer.h
@@ -13,11 +13,6 @@
 #  include <GL/glext.h>
 #endif
 
-#define RAIO_EIXO_X 0.4
-#define RAIO_EIXO_Y 0.2
-#define ALTURA_BASE 0.1
-#define RAIO_LED 0.02
-#define RAIO_POWER 0.04
 #define QUIT 3
 #define WIRED_MODE 5
 #define FILLED_MODE 6
